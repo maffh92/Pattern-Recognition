2051327ae2af976dee979c265857c50cd5813342
matthewswart92 <matthew.swart@hotmail.com>
2016-11-21 18:31:36 +0100

    Added a feature with multimodal

--- Exercise/Lab1/exper.R
+++ Exercise/Lab1/exper.R
@@ -9,17 +9,32 @@ inkDensity <- apply(digit.dat[,-1],1,sum)
 inkMeanStats <- tapply(inkDensity,digit.dat$label,mean)
 inkSdStats <- tapply(inkDensity,digit.dat$label,sd)
 
+#scale features
+ScaledinkSdStats <- scale(inkSdStats)
+names(ScaledinkSdStats) <- 0:9
+
+#combine features
+#assign to each label to correct ink feature
+inkFeature <- 1:nrow(digit.dat)
+for(i in 1:nrow(digit.train)){
+  inkFeature[i] <- ScaledinkSdStats[digit.train[i,]$label]
+}
+
+#combine the digit with the ink feature
+digit.ink <- cbind(digit.dat,inkFeature)
+
+
 #Multinomial Logit
 library(nnet)
 train.index <- c(1:1000)
 
 #initialise training and testData
 #training set
-digit.train <- digit.dat[train.index,]
+digit.train <- digit.ink[train.index,]
 digit.train$label = as.factor(digit.train$label)
 
 #test set
-digit.test <- digit.dat[-train.index,]
+digit.test <- digit.ink[-train.index,]
 digit.test$label = as.factor(digit.test$label)
 
 # fit multinomial logistic regression model
@@ -40,8 +55,7 @@ confmat <- table(digit.test$label,digit.multinom.test.pred)
 # use it to compute accuracy on test data
 sum(diag(confmat))/sum(confmat)
 
-
-#[,1]      [,2]      [,3]      [,4]      [,5]      [,6]      [,7]      [,8]      [,9]     [,10]
+#     [,1]      [,2]      [,3]      [,4]      [,5]      [,6]      [,7]      [,8]      [,9]     [,10]
 #[1,] 34632.408 15188.466 29871.099 28320.188 24232.722 25835.920 27734.917 22931.244 30184.148 24553.750
 #[2,]  8462.916  4409.932  7653.922  7574.975  6375.416  7527.595  7531.413  6169.042  7778.354  6466.003
 # This shows that the second digit (which is "1") can probably be distinguished from the rest with good accuracy based solely on this feature:
